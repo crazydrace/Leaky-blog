3825a25939cdba4128e47eb66c3ba5d89e8cc0f8
hisan <asssessment.hisan@gmail.com>
2024-12-04 08:45:14 +0530

    server detected

---
+++ react-app/src/App.jsx
@@ -0,0 +1,27 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import About from "./pages/About";
+import SignIn from "./pages/SignIn";
+import SignUp from "./pages/SignUp";
+import Projects from "./pages/Projects";
+import DashBoard from "./pages/DashBoard";
+import Header from "./components/Header";
+const App = () => {
+  return (
+    <>
+      <BrowserRouter>
+        <Header />
+        <Routes>
+          <Route path="/" element={<Home />}></Route>
+          <Route path="/sign-up" element={<SignUp />}></Route>
+          <Route path="/sign-in" element={<SignIn />}></Route>
+          <Route path="/projects" element={<Projects />}></Route>
+          <Route path="/about" element={<About />}></Route>
+          <Route path="/dashboard" element={<DashBoard />}></Route>
+        </Routes>
+      </BrowserRouter>
+    </>
+  );
+};
+
+export default App;
